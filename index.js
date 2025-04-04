de68e238ab85492e1ce197e3807abb84c8774709
TheoAdal <theoadal@gmail.com>
2025-04-04 16:24:17 +0300

    port on index

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use("/api", apiRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT} http://localhost:${PORT}`);
 });
 
 app.get("/test", (_req, res) => {
-    res.send("<h1>Dont mind me, just checking in :)</h1>");
-  });
+  res.send("<h1>Dont mind me, just checking in :)</h1>");
+});
